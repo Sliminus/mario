0b91e76274db42277d2a8e88f4e252c3fc22b1f4
Sliminus <103046552+Sliminus@users.noreply.github.com>
2023-05-02 15:02:32 +0000

    ehoh

--- mario pas guidé/main.c
+++ mario pas guidé/main.c
@@ -14,11 +14,14 @@ Le jeu sera quant à lui géré par le fichier game.c
 */
 int main(int argc, char *argv[])
 {
-    if(SDL_Init(SDL_INIT_VIDEO)==NULL)
+    if(SDL_Init(SDL_INIT_VIDEO)== -1)
     {
         perror("erreur");
         SDL_QUIT();
     }
+    //SDL_Window* SDL_CreateWindow(const char* title, int x, int y, int w, int h, Uint32 flags)
+    //SDL_Renderer* SDL_CreateRenderer(SDL_Window* window, int index, Uint32 flags)
+    //SDL_Texture* SDL_CreateTexture(SDL_Renderer* renderer, Uint32 format, int access, int w, int h)
 
 
     //SDL_Surface * IMG_Load(const char *file)
