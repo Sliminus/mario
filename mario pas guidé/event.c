7002ade1e5bfe747c2d50653260770eca0af1f9f
Sliminus <sliminusgamming@gmail.com>
2023-05-16 11:35:25 +0200

    test

--- mario pas guidé/event.c
+++ mario pas guidé/event.c
@@ -1,17 +1,1 @@
 #include "event.h"
-
-void event()
-{
-    int quit = 1;
-    while(quit)
-    {
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT)
-            {
-                quit = 0;
-                break;
-            }
-        }   
-    }
-}
--- mario pas guidé/game.c
+++ mario pas guidé/game.c
@@ -5,5 +5,20 @@
 #include "event.h"
 
 int jouer(SDL_Renderer *renderer) {
-    
+
+    int quit = 1;
+    while(quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                quit = 0;
+                break;
+            }
+        }   
+    }
+
+    return 0;
+
 }
