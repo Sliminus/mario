5be682e760e56903093852ff3aa253b46d8f59d3
Sevenlegarstropsympa <ramel.t@outlook.com>
2023-09-22 12:42:50 +0200

    mario

--- mario-code/charactere.c
+++ mario-code/charactere.c
@@ -7,8 +7,11 @@
 
 void creerPersonnage(Personnage* mario) {
     Personnage *mario = malloc(sizeof(Personnage));
-    mario->SDL_Texture** image=malloc(sizeof(SDL_Texture* image*NUMBER_IMAGE_MARIO));
-    mario->SDL_Rect={0,0,HEIGHT_MARIO,WIDTH_MARIO};
+    mario->image=malloc(sizeof(SDL_Texture** *NUMBER_IMAGE_MARIO));
+    mario->position.x=0;
+    mario->position.y=0;
+    mario->position.w=WIDTH_MARIO;
+    mario->position.h=HEIGHT_MARIO;
     mario->jump = 0;
     mario->jumptime=0;
     mario->gravite=0;
