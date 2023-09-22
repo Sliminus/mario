6168a5d041900a68e5095a7c5a317e25f50bc562
Sevenlegarstropsympa <ramel.t@outlook.com>
2023-09-22 11:53:30 +0200

    mario

--- mario-code/game.c
+++ mario-code/game.c
@@ -10,6 +10,7 @@ int jouer(SDL_Renderer *renderer) {
 
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
     Personnage *mario = malloc(sizeof(Personnage));
+    //charger map
     void creerPersonnage(Personnage* mario,Map* map);
     while(quit)
     {
